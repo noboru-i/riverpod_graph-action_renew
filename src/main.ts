33f3588f4960d50a502699c25895cc808884a616
noboru-i <ishikura.noboru@gmail.com>
2022-12-10 17:10:13 +0900

    run riverpod_graph

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,37 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import * as path from 'path'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // prepare
+    await exec.exec(
+      'dart pub global activate -sgit https://github.com/rrousselGit/riverpod.git --git-path packages/riverpod_graph'
+    )
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // execute
+    let outputs = ''
+    const workingDirectory = path.resolve(
+      process.env.GITHUB_WORKSPACE ?? '',
+      core.getInput('working-directory', {required: true})
+    )
+    const options: exec.ExecOptions = {cwd: workingDirectory}
+
+    options.listeners = {
+      stdout: data => {
+        outputs += data.toString()
+      }
+    }
+    await exec.exec(
+      'dart pub global run riverpod_graph:riverpod_graph $GITHUB_WORKSPACE > raw_output.md',
+      [],
+      options
+    )
+
+    const lines = outputs.trim().split(/\r?\n/)
+    for (const line of lines) {
+      core.debug(line)
+    }
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
